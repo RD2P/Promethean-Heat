cc4f06347adecdcb760f754f2fc4df756e48bc9f
Raphael <graphael.dr@gmail.com>
2025-09-27 19:32:35 -0600

    start scene

--- Assets/Scripts/LevelManagerScript.cs
+++ Assets/Scripts/LevelManagerScript.cs
@@ -16,7 +16,7 @@ public class LevelManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("script running");
     }
 
     // Update is called once per frame
