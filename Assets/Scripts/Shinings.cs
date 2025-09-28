6b9ff71fa3bfa7e3ea11e91e1411c69eb7f13b5d
Josh O <72672143+Weunbeatable@users.noreply.github.com>
2025-09-28 16:59:11 -0400

    script ref

--- Assets/Scripts/Shinings.cs
+++ Assets/Scripts/Shinings.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class Shinings : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public GameObject PlayerRef;
     void Start()
     {
-        
+        PlayerRef = GameObject.FindGameObjectWithTag()
     }
 
     // Update is called once per frame
