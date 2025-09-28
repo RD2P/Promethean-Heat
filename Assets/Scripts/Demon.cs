abb659d52eeb02f26a0c96fe19afd88657a4f6c4
Raphael <graphael.dr@gmail.com>
2025-09-27 20:57:07 -0600

    overworld tiling

--- Assets/Scripts/Demon.cs
+++ Assets/Scripts/Demon.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Demon : MonoBehaviour
 {
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (animator != null)
+        {
+            animator.SetBool("isFlying", true);
+        }
         
     }
 }
